439a61d4793150e319e73b361f6a7be36c5f8a47
Samuel Milhomens <samuel.mil.mar@gmail.com>
2024-04-19 07:13:59 -0300

    Login page

--- web/src/views/Login/index.jsx
+++ web/src/views/Login/index.jsx
@@ -1,6 +1,24 @@
+import "./login.css"
 
 export default function Login(){
     return (
-        <h1>Login page</h1>
+        <section className="login">
+            <form>
+                <h1>Efetuar Login</h1>
+                <div className="input_box">
+                    <label>Login:</label>
+                    <input type="text" />
+                </div>
+
+                <div className="input_box">
+                    <label>Senha:</label>
+                    <input type="password" />
+                </div>
+
+                <div className="input_box">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </section>
     )
 }
